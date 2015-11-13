c8da5cc2136326ebc6b83195f4a70c87cc324efe
ea <ea@gizal.org>
2015-11-13 23:05:25 +0100

    initial commit
    simple commands, all in python, no possible escaping of | or '\n\n'

---
+++ compile.py
@@ -0,0 +1,52 @@
+#!/bin/python
+
+from functools import reduce
+import sys
+
+if len(sys.argv) < 2:
+    print('./compile.py {yu source file}')
+    sys.exit(0)
+
+with open(sys.argv[1]) as f:
+    content = f.read()
+
+functions = {
+    '#': lambda *args: (print(*args), list(args))[1],
+    'reduce': lambda *args: [reduce(args[0], args[1:])],
+}
+
+operations = {
+    '+': '__add__', '*': '__mul__', '-': '__sub__', '/': '__div__',
+    '**': '__pow__', '//': '__floordiv__', '%': '__mod__', 'in': '__contains__'
+}
+
+def add_reducer(op, func):
+    functions[op] = lambda *args: C('reduce', [lambda x, y: getattr(x, func)(y)] + list(args))
+
+for op, func in operations.items():
+    add_reducer(op, func)
+
+def C(function, arguments):
+    return functions[function](*arguments)
+
+def expand(args):
+    for index, arg in enumerate(args):
+        if '..' in arg:
+            first, _, after = arg.partition('..')
+            second, _, after = after.partition('..')
+            args[index] = list(range(int(first), int(second)+1))
+        if arg.isdigit():
+            args[index] = int(arg)
+    return args
+
+for script in content.split('\n\n'):
+    script = script.strip()
+    if not script:
+        continue
+    ret = None
+    for code in script.split('|'):
+        tokens = code.split()
+        args = expand(tokens[1:])
+        if ret is not None:
+            args = ret + args
+        ret = C(tokens[0], args)
